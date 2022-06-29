6b3858ad8aab7f7b3daf29c5a02a26d13257ec12
chimpaji <chimpaji@outlook.com>
2022-06-29 19:29:11 +0100

    Backend with Pokemon data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,39 @@ import { trpc } from '@/utils/trpc';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const [first, secound] = getOptionsForVote();
+  const [ids, updateIds] = useState(getOptionsForVote());
+  const [first, secound] = ids;
+  const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: first }]);
+  const secoundPokemon = trpc.useQuery(['get-pokemon-by-id', { id: secound }]);
+  if (firstPokemon.isLoading) return <div>Loading....</div>;
+
   return (
     <div className='h-screen w-screen flex flex-col justify-center text-center items-center'>
-      <div className=''>Which Pokemon is the Roundest?</div>
-      <div className='border rounded p-8 flex justify-between max-w-2xl'>
-        <div className='w-16 h-16 bg-red-800'>{first}</div>
+      <div className='text-2xl'>Which Pokemon is the Roundest?</div>
+      <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
+        <div className='w-64 h-64 '>
+          <img
+            src={firstPokemon?.data?.sprites.front_default}
+            className='w-full'
+          />
+          <div className='text-xl text-center capitalize mt-[-2rem]'>
+            {firstPokemon?.data?.name}
+          </div>
+        </div>
         <div className='p-8'>vs</div>
-        <div className='w-16 h-16 bg-red-800'>{secound}</div>
+        <div className='w-64 h-64 '>
+          <img
+            src={secoundPokemon?.data?.sprites.front_default}
+            className='w-full'
+          />
+          <div className='text-xl text-center capitalize mt-[-2rem]'>
+            {secoundPokemon?.data?.name}
+          </div>
+        </div>
       </div>
     </div>
   );
